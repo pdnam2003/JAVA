8ad6cec86e5718032b505a254360708b72f4d16c
pdnam2003 <nam.pd.2446@aptechlearning.edu.vn>
2024-10-27 23:14:07 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,39 +1,31 @@
-import Controller.StaffController;
-import Controller.StudentController;
-import Entity.Gender;
-import Entity.Staff;
-import Entity.Student;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("c24635","hai", LocalDate.of(2000,4,6), Gender.MALE,"Ha Noi"));
-        students.add(new Student("c68957","Mai ",LocalDate.of(2001,1,13),Gender.OTHER,"Quang Ninh"));
-        students.add(new Student("c84682","Lam ",LocalDate.of(1999,4,1),Gender.FEMALE,"Ninh Binh"));
-
-        List<Staff> staffs = new ArrayList<>();
-        staffs.add(new Staff(1,"huy","cap 1",5000,Gender.FEMALE,"Ha Noi"));
-        staffs.add(new Staff(2,"hung","cap2",5500,Gender.FEMALE,"Ha Noi"));
-        staffs.add(new Staff(1,"nga","cap 3",5600,Gender.MALE,"Ha Noi"));
-
-        //hien thi thong tin hs tre nhat :
-        StudentController sc = new StudentController(students);
-        Optional<Student> studentYoungest = sc.getYoungest();
-        System.out.println(studentYoungest);
-        // tim hs theo ten
-        StudentController.getByName("hai").forEach(System.out::println);
-        //nv co tien luong cao nhat
-        StaffController sc2 = new StaffController(staffs);
-        Optional<Staff> staffMaxPay = sc2.getMaxPay();
-        System.out.println(staffMaxPay);
-
-
-
+      HashSet<Integer> newSet = new HashSet<Integer>();
+      newSet.add(10);
+      newSet.add(20);
+      newSet.add(30);
+      newSet.add(42);
+        for(Integer number : newSet){
+            if (divisionByThree(number)){
+                System.out.println(number);
+            }
+        }
+    }
+    public static boolean divisionByThree(int a){
+        int sum = 0;
+        while(a != 0){
+            sum = sum + a %10;
+            a/=10;
+        }
+        while (sum!= 0){
+            sum -=3 ;
+        }
+        return sum == 3;
     }
 
 }
