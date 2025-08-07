473bf6ab9ef8e17b0a5efd4a617a2ff3935c5374
pdnam2003 <nam.pd.2446@aptechlearning.edu.vn>
2025-08-07 10:37:40 +0700

    django

---
+++ student_project/studentapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gpa = models.FloatField()
+
+    def __str__(self):
+        return self.name
